7157b677419474b526fc836b344e292a60506689
abhishektiwari30 <Predator@MSI>
2020-07-22 19:19:02 +0530

    new commit

---
+++ src/main/java/com/at/services/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.at.services;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeService {
+
+	
+	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+	@ResponseBody
+	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+			throws JSONException {
+
+		JSONObject js = new JSONObject();
+		js.put("Name", "Riders Paradise");
+		js.put("Calling Name", "Rider");
+		js.put("DOB", "18-Jan-2018");
+		js.put("Hobbies", "Riding to new places..");
+		js.put("Places he like", "Laddakh and Spiti");
+
+		return js.toString();
+}
+}
